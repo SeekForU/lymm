628d2cea6f7fe45b38befebe63b4a12399f8a6ef
SeekForU <1014318193@qq.com>
2017-03-30 09:29:21 +0800

    3.30

--- src/com/lymm/servlet/OrdersServlet.java
+++ src/com/lymm/servlet/OrdersServlet.java
@@ -9,12 +9,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by ${Seek} on 2017/3/28
@@ -42,15 +44,81 @@ public class OrdersServlet extends HttpServlet {
 		}
 	}
 
+	/**
+	 * 预约列表
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	protected void orderList(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+
+
+		HttpSession httpSession = request.getSession();
+		String trueName = (String) httpSession.getAttribute("trueName");
+		//TODO
+		List<Orders> ordersList = ordersDAO.getOrders(trueName);
+		request.setAttribute("ordersList", ordersList);
+		//版本1
+		//request.getRequestDispatcher("orderList.jsp").forward(request,response);
+		// 版本2
+		request.getRequestDispatcher("pages/orderList.jsp").forward(request,response);
+	}
+
+	/**
+	 * 取消预约
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	private void cancel(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		String idStr = request.getParameter("id");
+		int id = -1;
+
+		try {
+			id = Integer.parseInt(idStr);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		ordersDAO.deleteOrder(id);
+		//HttpSession session=request.getSession();
+		//	String trueName=(String)session.getAttribute("trueName");
+		List<Orders> ordersList = ordersDAO.getOrders("Hello");
+		request.setAttribute("ordersList", ordersList);
+		//版本1
+		//request.getRequestDispatcher("orderList.jsp").forward(request,response);
+		//版本2
+		request.getRequestDispatcher("pages/orderList.jsp").forward(request,response);
+	}
+
+
+	/**
+	 * 增加预约记录
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
 	protected void order(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		//String trueName = request.getParameter("trueName");
-		String trueName = "Hello";
+
+		request.setCharacterEncoding("UTF-8");
+
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+
+		String trueName = request.getParameter("trueName");
+		//String trueName = "Hello";
 		String name = request.getParameter("name");
 		String type = request.getParameter("type");
 		String time = request.getParameter("time");
-		System.out.println(time);
+		//System.out.println(time);
 
+		System.out.println(type);
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
 		PrintWriter out = response.getWriter();
 		try {
@@ -59,22 +127,11 @@ public class OrdersServlet extends HttpServlet {
 			System.out.println(orders);
 			if (orders != null) {
 				ordersDAO.AddOrders(orders);
-				out.print("<script type='text/javascript'>alert('Order Successful');window.location='doctors.jsp';</script>");
+				//out.print("<script type='text/javascript'>alert('Order Successful');window.location='doctors.jsp';</script>");
+				out.print("<script type='text/javascript'>alert('Order Successful');window.location='pages/doctor.jsp';</script>");
 			}
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-
-
-		//response.setContentType("text/html;charset=GBK");
-		//PrintWriter out = response.getWriter();
-		//if (order.addOrder(tureName, type, time, name)) {
-		//	out.print("<script type='text/javascript'>alert('预定成功');window.location='home.jsp';</script>");
-		//} else {
-		//
-		//	out.print("<script type='text/javascript'>alert('预定失败');history.go(-1);</script>");
-		//}
 	}
-
-
 }
